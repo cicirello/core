aa48217171a5e4c38e2e19d0b78509406c2cfa18
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-09-15 12:49:10 -0400

    removed unused import

--- src/main/java/org/cicirello/ds/SimpleBinaryHeapDouble.java
+++ src/main/java/org/cicirello/ds/SimpleBinaryHeapDouble.java
@@ -24,7 +24,6 @@ package org.cicirello.ds;
 
 import java.lang.reflect.Array;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
